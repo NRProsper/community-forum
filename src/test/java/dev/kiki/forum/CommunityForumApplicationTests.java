61c4e1e8bf8f15ce35a70284a07a1dde16c7296e
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-11-22 15:43:26 +0200

    chore: project initialization

---
+++ src/test/java/dev/kiki/forum/CommunityForumApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kiki.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
